4bfcb5f099ae30919639f9d3d5e93c5dd5594902
ygkn <2000ygkn0713@gmail.com>
2020-08-30 12:30:42 +0900

    みやすく

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url =
   "https://script.googleusercontent.com/macros/echo?user_content_key=QSl-iFVncWXotiQb03xeZl8kX7v39Rv2Qvlm-WKW4XFN53JLLrVIx47XYYllxh3hhWL7VhPraDKTyW6HCIXEqH7_oHc0XITcm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLEnblhtnYYl6WGFfT3ma0FMI8sjjbCJfxL5LANA_LhV1LEPXBLZOjqcrAGr2lGEy5cdv3aNHr6K&lib=MoGUgZmNVO0EzaTnPiIvmE4yCk3cQEqvu";
+
 new Vue({
   el: "#app",
   data() {
